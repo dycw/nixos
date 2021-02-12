bf306da8c5041c06a4c7011f75755732598f9bdc
Derek Wan <d.wan@icloud.com>
2021-02-12 11:52:35 +0800

    Add bat

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -68,6 +68,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    bat
     curl
     gitAndTools.delta
     exa
