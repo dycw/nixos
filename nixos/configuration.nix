7dcee2d9e065a3b817f61a198571f58f317584b8
Derek Wan <d.wan@icloud.com>
2021-02-12 12:45:00 +0800

    Add dropbox

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -72,6 +72,7 @@
     curl
     gitAndTools.delta
     diskonaut
+    dropbox-cli
     exa
     fd
     firefox
@@ -92,6 +93,28 @@
     zoxide
     zsh
   ];
+  nixpkgs.config.allowUnfree = true;
+
+
+  systemd.user.services.dropbox = {
+    description = "Dropbox";
+    after = [ "xembedsniproxy.service" ];
+    wants = [ "xembedsniproxy.service" ];
+    wantedBy = [ "graphical-session.target" ];
+    environment = {
+      QT_PLUGIN_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtPluginPrefix;
+      QML2_IMPORT_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtQmlPrefix;
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.dropbox.out}/bin/dropbox";
+      ExecReload = "${pkgs.coreutils.out}/bin/kill -HUP $MAINPID";
+      KillMode = "control-group"; # upstream recommends process
+      Restart = "on-failure";
+      PrivateTmp = true;
+      ProtectSystem = "full";
+      Nice = 10;
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
